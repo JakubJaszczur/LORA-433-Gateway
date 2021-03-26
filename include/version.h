680dd631846fb068be33f50836dfc8b8fa803089
Jakub Jaszczur <69448053+JakubJaszczur@users.noreply.github.com>
2021-03-26 12:10:08 +0100

    Change version number

--- include/version.h
+++ include/version.h
@@ -3,8 +3,8 @@
   #define BUILD_NUMBER "3"
 #endif
 #ifndef VERSION
-  #define VERSION "v0.1.210326_3 - 2021-03-26 10:59:25.512637"
+  #define VERSION "v1.1.210326_3 - 2021-03-26 10:59:25.512637"
 #endif
 #ifndef VERSION_SHORT
-  #define VERSION_SHORT "v0.1.210326_3"
+  #define VERSION_SHORT "v1.1.210326_3"
 #endif
